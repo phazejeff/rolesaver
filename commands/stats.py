5b994e35ff50f08d0933af7541ee3840e7201f60
phazejeff <85849384+phazejeff@users.noreply.github.com>
2023-12-19 15:15:01 -0800

    add log stats

--- commands/stats.py
+++ commands/stats.py
@@ -3,6 +3,8 @@ from bot import rolesaver
 
 @rolesaver.tree.command(name="stats", description="Shows the bot's global and server stats")
 async def stats(interaction: discord.Interaction):
+    log = rolesaver.database.fetch_log(interaction.guild)
+
     await interaction.response.send_message(f"""
 ## Global Stats
 Server Count: **{len(rolesaver.guilds)}**
@@ -17,4 +19,6 @@ Shard ID: **{interaction.guild.shard_id}**
 Shard Latency: **{round(rolesaver.get_shard_latency(interaction.guild.shard_id) * 1000)} ms**
 Members Saved: **{rolesaver.database.member_count(interaction.guild)}**
 Unique Roles Saved: **{rolesaver.database.guild_role_count(interaction.guild)}**
+Logging: **{log.is_logging}**
+Logging Channel: **{'<#' + str(log.log_channel) + '>' if log.is_logging else None}**
 """)
