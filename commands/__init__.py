7155128d3b64c069aaf463e8fb56c0067e1de0a8
phazejeff <85849384+phazejeff@users.noreply.github.com>
2023-12-22 14:11:19 -0800

    fix init

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,4 +4,4 @@ from .whitelist import whitelist
 from .forcesave import forcesave
 from .forcerestore import forcerestore
 from .stats import stats
-from .logging import logging
+from .loggingcommand import logging
