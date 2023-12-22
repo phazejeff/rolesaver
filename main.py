ab1a9b8634bb3218b24919a067e9392bbdd9d575
phazejeff <85849384+phazejeff@users.noreply.github.com>
2023-12-22 14:11:32 -0800

    comment out load dotenv

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from bot import rolesaver
 import commands
 import events
 import context
-load_dotenv() # loads token from local .env file. when in production, ill probably just use docker
+# load_dotenv() # loads token from local .env file. when in production, ill probably just use docker
 
 rolesaver.run()
