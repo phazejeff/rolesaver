7c13deb130f0dd386ea6deda1e4cef0bed9b40f7
phazejeff <85849384+phazejeff@users.noreply.github.com>
2024-01-10 12:47:30 -0800

    Update stats command for DM use

--- commands/stats.py
+++ commands/stats.py
@@ -2,12 +2,9 @@ import discord
 from bot import rolesaver
 
 @rolesaver.tree.command(name="stats", description="Shows the bot's global and server stats")
-@discord.app_commands.guild_only()
 async def stats(interaction: discord.Interaction):
     await interaction.response.defer(thinking=True)
-    log = rolesaver.database.fetch_log(interaction.guild)
-
-    await interaction.followup.send(f"""
+    msg = f"""
 ## Global Stats
 Server Count: **{len(rolesaver.guilds)}**
 Members Saved: **{rolesaver.database.nickname_count()}**
@@ -15,7 +12,11 @@ Users Saved: **{rolesaver.database.user_count()}**
 Unique Roles Saved: **{rolesaver.database.role_count()}**
 Shard Count: **{rolesaver.shard_count}**
 Latency Average: **{round(rolesaver.latency * 1000)} ms**
-
+"""
+    
+    if interaction.guild:
+        log = rolesaver.database.fetch_log(interaction.guild)
+        msg += f"""
 ## Server Stats
 Shard ID: **{interaction.guild.shard_id}**
 Shard Latency: **{round(rolesaver.get_shard_latency(interaction.guild.shard_id) * 1000)} ms**
@@ -23,4 +24,6 @@ Members Saved: **{rolesaver.database.member_count(interaction.guild)}**
 Unique Roles Saved: **{rolesaver.database.guild_role_count(interaction.guild)}**
 Logging: **{log.is_logging}**
 Logging Channel: **{'<#' + str(log.log_channel) + '>' if log.is_logging else None}**
-""")
+"""
+
+    await interaction.followup.send(msg)
