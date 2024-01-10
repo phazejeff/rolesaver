3e9644f7d00f870744e890b62afd692eae545109
phazejeff <85849384+phazejeff@users.noreply.github.com>
2024-01-10 12:13:48 -0800

    remove unnessesary comment from main

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from bot import rolesaver
 import commands
 import events
 import context
-# load_dotenv() # loads token from local .env file. when in production, ill probably just use docker
+# load_dotenv() # loads token from local .env file. 
 
 rolesaver.run()
