36834b543c77bbaa1db673d0aba2313056c00c6f
phazejeff <85849384+phazejeff@users.noreply.github.com>
2024-01-15 12:44:30 -0800

    adjust help for premium

--- commands/help.py
+++ commands/help.py
@@ -5,14 +5,13 @@ from bot import rolesaver
 async def help(interaction: discord.Interaction):
     await interaction.response.defer()
     await interaction.followup.send(f"""
-**Thanks for adding Role Saver to your server!**
-Remember to put the `Role Saver` role above your other roles, otherwise it cannot access them.
+**Thanks for adding Role Saver Premium to your server!**
+In order to activate, just use /activate in the server you want the bot activated in. You can only have 1 server activated per Discord account
+
+Remember to put the `Role Saver Premium` role above your other roles, otherwise it cannot access them.
 
 By default, this will save every role a user has. If you want to adjust that, you can use either /blacklist or /whitelist respectively.
                        
-If you need any help, you can join the support server and talk to the developer directly: https://discord.gg/gKnRsMaBZ3
-
-__**Keep in mind that this is a public bot!**__
-That means during periods of high usage, the bot may lose functionality as it hits Discord's rate limit. If you are on a server that needs high reliability, you might want to consider using the Premium bot located here: https://patreon.com/rolesaver                     
+If you need any help, you can join the support server and talk to the developer directly: https://discord.gg/gKnRsMaBZ3                   
 """
     )
