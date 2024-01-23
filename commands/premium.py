12c9385b94da0be9d875b4c547c98d76ed0b60b5
phazejeff <85849384+phazejeff@users.noreply.github.com>
2024-01-22 19:01:01 -0800

    adjust premium message

--- commands/premium.py
+++ commands/premium.py
@@ -6,7 +6,8 @@ async def premium(interaction: discord.Interaction):
     await interaction.response.defer()
     await interaction.followup.send(f"""
 __**This is a public bot!**__
-That means during periods of high usage, the bot may lose functionality as it hits Discord's rate limit. If you are on a server that needs high reliability, you might want to consider using the Premium bot located here: https://patreon.com/rolesaver
+That means during periods of high usage, the bot may lose functionality as it hits Discord's rate limit. For example, you may notice that you sometimes need to run a command multiple times because the bot didn't respond to the command the first time. If you are on a server that needs high reliability, you might want to consider using the Premium bot located here: https://patreon.com/rolesaver
+                                    
 Once the premium instance hits 100 servers, another premium instance will be avaliable to use to avoid bottlenecks. This makes the premium instances far more reliable than the public bot.
 """
     )
